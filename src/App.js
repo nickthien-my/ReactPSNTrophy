2b2dbeb5dd09ab7fca341794baf8a2dd8f2ca184
nickthien-my <84914767+nickthien-my@users.noreply.github.com>
2022-10-11 19:09:15 +0800

    11/10/22

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import PokemonList from "./components/PokemonList";
+import axios from "axios";
+import Pagination from "./components/Pagination";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setPrevPageUrl(res.data.previous)
+      setNextPageUrl(res.data.next)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+  }, [currentPageUrl])
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  if (loading) return "Loading..."
+
   return (
-    <div className="App">
-      <h2>Hello, world! Text</h2>
-      <div class="navbar">
-        <ul style={{ color: "red" }}>
-          <li>Home</li>
-          <li>Projects</li>
-        </ul>
-        <img src="https://carewarriors.org.my/logo.png" alt="logo"></img>
-      </div>
-    </div >
+    <>
+      <div>Hello, world!</div>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+      />
+      <Button>Press Me!</Button>
+    </>
   );
 }
 
